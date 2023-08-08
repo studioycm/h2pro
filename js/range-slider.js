33ed509919f085445a0ca3d9e2b43f9b3c9fddc0
appitsolution <applicationitsolution@gmail.com>
2023-08-08 11:46:05 +0300

    fix js

--- js/range-slider.js
+++ js/range-slider.js
@@ -1,12 +1,18 @@
 const rangeSlider = (element, min, max, meaning = "%") => {
-  const currentValue = (Number(element.value) / max) * 100;
-  // console.log(element.value - min);
+  // const currentValue = (Number(element.value) / max) * 100;
+  const currentValue = (
+    ((Number(element.value) - min) / (max - min)) *
+    100
+  ).toFixed(2);
+
+  console.dir(element.getBoundingClientRect().width);
 
   const backgroundLinear = `
     linear-gradient(to right, rgb(114, 190, 68) 0%, rgb(139, 223, 89) ${currentValue}%, rgb(239, 242, 245) ${currentValue}%, rgb(239, 242, 245) 100%)`;
 
   element.style.background = backgroundLinear;
 
+  element.parentElement.children[2].style.left = `${currentValue}%`;
   element.parentElement.children[2].children[0].innerText = `${Number(
     element.value
   )}${meaning}`;
