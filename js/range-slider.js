dcc321b5415acf336ff20905775943ddbfc410e0
studioycm <88206928+studioycm@users.noreply.github.com>
2023-09-09 04:00:26 +0300

    minor cleanup code, merging else if conditions

--- js/range-slider.js
+++ js/range-slider.js
@@ -1,77 +1,53 @@
 const rangeSlider = (element, meaning = "%") => {
-  const min = Number(element.min);
-  let max = Number(element.max);
   const hoursCap = 8760;
+  const min = Number(element.min);
+  // changed max from const to let, so that it can be changed if "meaning" = hrs
+  let max = Number(element.max);
+  // get initial input element value
   let elValue = Number(element.value);
+  // if meaning is hrs, set max at 8760 hours, and cap elValue at 8760.
   if (meaning === "hrs") {
     elValue = Number(element.value) >= 8760 ? 8760 : Number(element.value);
     max = hoursCap;
   }
-
-
-  const currentValue = ((elValue - min) / (max - min)) * 100;
+  // parse value to a 
+  const percentageValue = ((elValue - min) / (max - min)) * 100;
   
-  console.log("meaning");
-  console.log(meaning);
-  console.log("min: " + min, "max: " + max);
-  console.log("element.value");
-  console.log(element.value);
-  console.log("elValue after cap");
-  console.log(elValue);
-  console.log("currentValue");
-  console.log(currentValue);
-  console.log("label before");
-  console.log(element.parentElement.children[2].children[0].innerText);
-  console.log("label now");
-  console.log(elValue);
+  // console.log("meaning");
+  // console.log(meaning);
+  // console.log("min: " + min, "max: " + max);
+  // console.log("element.value");
+  // console.log(element.value);
+  // console.log("elValue after cap");
+  // console.log(elValue);
+  // console.log("percentageValue");
+  // console.log(percentageValue);
+  // console.log("label before");
+  // console.log(element.parentElement.children[2].children[0].innerText);
+  // console.log("label now");
+  // console.log(elValue);
+
   const backgroundLinear = `
-    linear-gradient(to right, rgb(114, 190, 68) 0%, rgb(139, 223, 89) ${currentValue}%, rgb(239, 242, 245) ${currentValue}%, rgb(239, 242, 245) 100%)`;
+    linear-gradient(to right, rgb(114, 190, 68) 0%, rgb(139, 223, 89) ${percentageValue}%, rgb(239, 242, 245) ${percentageValue}%, rgb(239, 242, 245) 100%)`;
 
   element.style.background = backgroundLinear;
 
-  element.parentElement.children[2].style.left = `${currentValue}%`;
-  element.parentElement.children[3].style.left = `${currentValue}%`;
+  element.parentElement.children[2].style.left = `${percentageValue}%`;
+  element.parentElement.children[3].style.left = `${percentageValue}%`;
 
-  if (meaning === "kg") {
-    element.parentElement.children[2].children[0].innerText = `$${Number(
-      element.value
-    )} / kg`;
-  } else if (meaning === "MWh") {
-    element.parentElement.children[2].children[0].innerText = `$${Number(
-      element.value
-    )} / MWh`;
-  } else if (meaning === "hrs") {
-    element.parentElement.children[2].children[0].innerText = `${elValue} hrs`;
-    console.log("'hrs' label now");
-    console.log(element.parentElement.children[2].children[0].innerText);
+  if (meaning === "kg" || meaning === "MWh") {
+    element.parentElement.children[2].children[0].innerText = `$${elValue} / ${meaning}`;
   } else {
-    element.parentElement.children[2].children[0].innerText = `${Number(
-      element.value
-    )} ${meaning}`;
+    element.parentElement.children[2].children[0].innerText = `${elValue} ${meaning}`;
   }
 };
 
-const checkValue = (rangeInput) => {
-  // console.log(rangeInput.value);
-  // if (Number(rangeInput.value) >= 8500) {
-  //   rangeInput.setAttribute("step", "260");
-  // } else if (
-  //   Number(rangeInput.value) !== 8760 &&
-  //   Number(rangeInput.value) > 8500
-  // ) {
-  //   rangeInput.setAttribute("step", "500");
-  // } else {
-  //   rangeInput.setAttribute("step", "500");
-  // }
-  // console.log(rangeInput.step);
-};
 
 document
   .getElementById("track1")
   .addEventListener("input", ({ target }) => rangeSlider(target, "MW"));
 
 document.getElementById("track2").addEventListener("input", ({ target }) => {
-  checkValue(target);
   rangeSlider(target, "hrs");
 });
 
